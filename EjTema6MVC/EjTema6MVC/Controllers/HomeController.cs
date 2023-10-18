8ee6f4011dc93b5f6fa75764657689c85bc7baa2
juanpc19 <juan.perez@iesnervion.es>
2023-10-18 14:43:11 +0200

    revisar html

---
+++ EjTema6MVC/EjTema6MVC/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EjTema6MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        //hola mundo
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EjTema6MVC/EjTema6MVC/Controllers/ProductosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EjTema6MVC.Controllers
+{
+    public class ProductosController : Controller
+    {
+      
+        public IActionResult ListadoProductos()
+        {
+            return View();
+        }
+
+    }
+}
