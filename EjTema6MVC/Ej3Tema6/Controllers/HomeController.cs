01e79ceec7399229f00d1b663adc29c5c06b169b
juanpc19 <juan.perez@iesnervion.es>
2023-10-19 10:49:05 +0200

    a

---
+++ EjTema6MVC/Ej3Tema6/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Ej3Tema6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ej3Tema6.Controllers
+{
+    public class HomeController : Controller
+    {
+   
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        
+    }
+}
---
+++ EjTema6MVC/Ej3Tema6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ej3Tema6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
