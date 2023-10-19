c9065985c4a09ccd9c4a5bec14febfc734084c13
juanpc19 <juan.perez@iesnervion.es>
2023-10-19 18:03:01 +0200

    ej 4 hecho

---
+++ EjTema6MVC/Ej4Tema6/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Ej4Tema6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ej4Tema6.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
