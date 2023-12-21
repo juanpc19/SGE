831ad395738848d3f0e0588c4f052dbcbe4fa7a9
juanpc19 <juan.perez@iesnervion.es>
2023-12-21 11:51:14 +0100

    dada

---
+++ ExamenDulceNavidad/ExamenDulceNavidad/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ExamenDulceNavidad.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExamenDulceNavidad.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+       public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
