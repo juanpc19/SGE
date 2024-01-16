32f519daf4a2a064a26741bfd6bae12a9d846d77
juanpc19 <juan.perez@iesnervion.es>
2024-01-16 22:50:29 +0100

    d

---
+++ JavaScriptTema12/EjerciciosTema12/Ej3/clsListadoMarcas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Coche
+{
+    public int Id { get; set; }
+    public string Marca { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Crear una lista de marcas de coches
+        List<Coche> listaDeCoches = new List<Coche>();
+
+        // Agregar algunas marcas de coches a la lista
+        listaDeCoches.Add(new Coche { Id = 1, Marca = "Toyota" });
+        listaDeCoches.Add(new Coche { Id = 2, Marca = "Honda" });
+        listaDeCoches.Add(new Coche { Id = 3, Marca = "Ford" });
+        listaDeCoches.Add(new Coche { Id = 4, Marca = "Chevrolet" });
+
+      
+    }
+}
---
+++ JavaScriptTema12/EjerciciosTema12/Ej3/clsListadoModelos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class clsListadoModelos
+{
+    public static List<string> GetListado()
+    {
+        List<string> marcasDeCoches = new List<string>
+            {
+                "Toyota",
+                "Honda",
+                "Ford",
+                "Chevrolet"
+            };
+
+        return marcasDeCoches;
+    }
+}
